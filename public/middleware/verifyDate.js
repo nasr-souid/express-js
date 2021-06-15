e035e4ab8cffc96b1fe0006e2f5e5b8fca17ae1d
HP <nasr.souid1995@gmail.com>
2021-06-15 11:33:25 +0100

    adding middleware

---
+++ public/middleware/verifyDate.js
@@ -0,0 +1,12 @@
+let hour = new Date().getHours()
+let day=new Date().getDay()
+
+function verifyDate(req, res, next){
+    if (8 <hour && hour < 18 && day!==0 && day!==6 ){
+        next()
+    }
+    else{
+        alert("This web application is only available during working hours (Monday to Friday,  from 9 to 17)")
+    }
+}
+module.exports = verifyDate
--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 const express = require('express')
 const app = express()
 let ejs = require('ejs')
-const port = 3000
-// let hour = new Date().getHours()
-// let day=new Date().getDay()
+const port = 5432
+const verifyDate=require('./public/middleware/verifyDate')
 
-// const verifyDate=fonctionfunction(req, res, next){
-//     if ((8 <hour < 18)&&(day!==0)&&(day!==6)){
-//         next()
-//     }
-//     else{
-//         alert("This web application is only available during working hours (Monday to Friday,  from 9 to 17)")
-//     }
-// }
-// app.use(verifyDate)
-app.use( express.static( "public" ) )
+app.use(express.static(__dirname + "/public" ) )
 app.set('view engine', 'ejs');
 app.set('views','./views');
+app.use(verifyDate)
 
 app.get('/', function(req, res,) {
     res.render('pages/home');
@@ -24,30 +15,11 @@ app.get('/', function(req, res,) {
 app.get('/about', function(req, res,) {
     res.render('pages/about');
 });
-app.get('/contactus', function(req, res,) {
-    res.render('pages/contactus');
+app.get('/contactUs', function(req, res,) {
+    res.render('pages/contactUs');
 });
-// app.get('/', (req, res) => {
-//   res.send('Hello Worljmkllkkjgvcckkkkkkkjjjd!')
-// })
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-// var express = require('express')
-// var app = express()
-
-// app.set('view engine', 'ejs');
-// app.set('views','./views');
-
-// app.get('/', function(req, res) {
-//     res.render('pages/home');
-// });
-// app.get('/about', function(req, res) {
-//     res.render('pages/about');
-// });
-// app.get('/contact us', function(req, res) {
-//     res.render('pages/contact us');
-// });
-
-// app.listen(3000)
